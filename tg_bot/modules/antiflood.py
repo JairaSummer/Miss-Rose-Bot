68584b62232e1c24857aea200bbc177f3aff1516
JairaSummer <89501904+JairaSummer@users.noreply.github.com>
2021-10-02 00:34:30 +0800

    Update antiflood.py

--- tg_bot/modules/antiflood.py
+++ tg_bot/modules/antiflood.py
@@ -14,125 +14,8 @@ from tg_bot.modules.helper_funcs.string_handling import extract_time
 from tg_bot.modules.log_channel import loggable
 from tg_bot.modules.sql import antiflood_sql as sql
 
-def mute(bot: Bot, update: Update, args: List[str]) -> str:
-    chat = update.effective_chat  # type: Optional[Chat]
-    user = update.effective_user  # type: Optional[User]
-    message = update.effective_message  # type: Optional[Message]
-
-    user_id = extract_user(message, args)
-    if not user_id:
-        message.reply_text("You'll need to either give me a username to mute, or reply to someone to be muted.")
-        return ""
-
-    if user_id == bot.id:
-        message.reply_text("I'm not muting myself!")
-        return ""
-
-    member = chat.get_member(int(user_id))
-
-    if member:
-        if is_user_admin(chat, user_id, member=member):
-            message.reply_text("Afraid I can't stop an admin from talking!")
-
-        elif member.can_send_messages is None or member.can_send_messages:
-            bot.restrict_chat_member(chat.id, user_id, can_send_messages=False)
-            message.reply_text("👍🏻 muted! 🤐")
-            return "<b>{}:</b>" \
-                   "\n#MUTE" \
-                   "\n<b>Admin:</b> {}" \
-                   "\n<b>User:</b> {}".format(html.escape(chat.title),
-                                              mention_html(user.id, user.first_name),
-                                              mention_html(member.user.id, member.user.first_name))
-
-        else:
-            message.reply_text("This user is already muted!")
-    else:
-        message.reply_text("This user isn't in the chat!")
-
-    return ""
 
 FLOOD_GROUP = 3
-
-@run_async
-@bot_admin
-@can_restrict
-@user_admin
-@loggable
-def temp_mute(bot: Bot, update: Update, args: List[str]) -> str:
-    chat = update.effective_chat  # type: Optional[Chat]
-    user = update.effective_user  # type: Optional[User]
-    message = update.effective_message  # type: Optional[Message]
-
-    user_id, reason = extract_user_and_text(message, args)
-
-    if not user_id:
-        message.reply_text("You don't seem to be referring to a user.")
-        return ""
-
-    try:
-        member = chat.get_member(user_id)
-    except BadRequest as excp:
-        if excp.message == "User not found":
-            message.reply_text("I can't seem to find this user")
-            return ""
-        else:
-            raise
-
-    if is_user_admin(chat, user_id, member):
-        message.reply_text("I really wish I could mute admins...")
-        return ""
-
-    if user_id == bot.id:
-        message.reply_text("I'm not gonna MUTE myself, are you crazy?")
-        return ""
-
-    if not reason:
-        message.reply_text("You haven't specified a time to mute this user for!")
-        return ""
-
-    split_reason = reason.split(None, 1)
-
-    time_val = split_reason[0].lower()
-    if len(split_reason) > 1:
-        reason = split_reason[1]
-    else:
-        reason = ""
-
-    mutetime = extract_time(message, time_val)
-
-    if not mutetime:
-        return ""
-
-    log = "<b>{}:</b>" \
-          "\n#TEMP MUTED" \
-          "\n<b>Admin:</b> {}" \
-          "\n<b>User:</b> {}" \
-          "\n<b>Time:</b> {}".format(html.escape(chat.title), mention_html(user.id, user.first_name),
-                                     mention_html(member.user.id, member.user.first_name), time_val)
-    if reason:
-        log += "\n<b>Reason:</b> {}".format(reason)
-
-    try:
-        if member.can_send_messages is None or member.can_send_messages:
-            bot.restrict_chat_member(chat.id, user_id, until_date=mutetime, can_send_messages=False)
-            message.reply_text("shut up! 😠 Muted for {}!".format(time_val))
-            return log
-        else:
-            message.reply_text("This user is already muted.")
-
-    except BadRequest as excp:
-        if excp.message == "Reply message not found":
-            # Do not reply
-            message.reply_text("shut up! 😠 Muted for {}!".format(time_val), quote=False)
-            return log
-        else:
-            LOGGER.warning(update)
-            LOGGER.exception("ERROR muting user %s in chat %s (%s) due to %s", user_id, chat.title, chat.id,
-                             excp.message)
-            message.reply_text("Well damn, I can't mute that user.")
-
-    return ""
-
 
 @run_async
 @loggable
@@ -140,7 +23,6 @@ def check_flood(bot: Bot, update: Update) -> str:
     user = update.effective_user  # type: Optional[User]
     chat = update.effective_chat  # type: Optional[Chat]
     msg = update.effective_message  # type: Optional[Message]
-
     if not user:  # ignore channels
         return ""
 
@@ -154,7 +36,7 @@ def check_flood(bot: Bot, update: Update) -> str:
         return ""
 
     try:
-        bot.restrict_chat_member(chat.id, user_id, can_send_messages=False)
+        bot.restrict_chat_member(chat.id, user.id, can_send_messages=False)
         msg.reply_text("Shhhhh...")
 
         return "<b>{}:</b>" \
